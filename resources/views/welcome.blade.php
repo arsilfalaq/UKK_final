b82cdb8334103110cf1c27895ddb264590a936fd
arsilfalaq <arsylfalaq@gmail.com>
2025-02-04 09:25:26 +0700

    Pesan commit ke 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -116,12 +116,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
+                   
                 </ul>
             </div>
         </div>
